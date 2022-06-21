0365da257ae6b2de21034fc781e93386cf6c60ba
dprincecoder <dprincecoder@gmail.com>
2022-06-22 00:00:09 +0100

    fix

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 // program for a simple calculator
 
 // take the  first operand input
-const number1 = parseFloat(prompt('Enter first number: '));
+const number1 = parseFloat(prompt("Enter first number: "));
 
 // take the operator input
-const operator = prompt('Enter operator ( either +, -, * or / ): ');
+const operator = prompt("Enter operator ( either +, -, *, (**, ^) or / ): ");
 
 // take the second operand input
-const number2 = parseFloat(prompt('Enter second number: '));
+const number2 = parseFloat(prompt("Enter second number: "));
 
 let result;
 
 // using if...else if... else
-if (operator == '+') {
-    result = number1 + number2;
-}
-else if (operator == '-') {
-    result = number1 - number2;
-}
-else if (operator == '*') {
-    result = number1 * number2;
-}
-else {
-    result = number1 / number2;
+if (operator == "+") {
+  result = number1 + number2;
+} else if (operator == "-") {
+  result = number1 - number2;
+} else if (operator == "*") {
+  result = number1 * number2;
+} else if (operator == "**") {
+  result = Math.pow(number1, number2);
+} else if (operator == "^") {
+  result = Math.pow(number1, number2);
+} else {
+  result = number1 / number2;
 }
 
 // display the result
